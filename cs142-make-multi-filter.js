2a7f5dbe79069e9e4032fa96c3eae4610f0e4222
Vitalie Paraschiv <vitalieparaskiv@gmail.com>
2020-12-27 21:45:43 +0200

    project #2 part 1, 2

--- cs142-make-multi-filter.js
+++ cs142-make-multi-filter.js
@@ -1,26 +1,53 @@
 function cs142MakeMultiFilter(originalArray) {
 
-    function arrayFilterer(filterCriteria, callback) {
-        const currentArray = originalArray;
+    let currentArray = originalArray.slice(); 
+
+    return function arrayFilterer(filter, call) {
+
+        if (!isDefinedFunction(filter)) {
+            return currentArray;
+        }
+
         for (let index = 0; index < currentArray.length; index++) {
             const element = currentArray[index];
-            if (filterCriteria(element) === false) {
+            if (filter(element) === false) {
                 currentArray.splice(index, 1);
             }
         }
-        return callback();
+
+        if (isDefinedFunction(call)) {
+            const boundCallback = callback.bind(originalArray);
+            boundCallback(currentArray);
+        }
     }
-    return arrayFilterer;
 }
 
-function filterCriteria(element) {
-    return element !== 2;
-}
+// function filterCriteria(element) {}
 
 function callback(currentArray) {
-    console.log(this); 
-    console.log(currentArray); 
+    console.log(this);
+    console.log(currentArray);
 }
 
+function isDefinedFunction(func) {
+    return func !== undefined || typeof (func) === 'function'
+}
+
+module.exports = cs142MakeMultiFilter;
+
 var arrayFilterer1 = cs142MakeMultiFilter([1, 2, 3]);
-console.log(arrayFilterer1);
+arrayFilterer1(function (elem) {
+    return elem !== 3; // check if element is not equal to 2
+});
+console.log(arrayFilterer1());
+// arrayFilterer1(function (elem) {
+//     return elem !== 3; // check if element is not equal to 2
+// }, function (currentArray) {
+//     console.log(this); // printing 'this' within the callback function should print originalArray which is [1,2,3]
+//     console.log(currentArray); // prints [1, 3]
+// });
+// arrayFilterer1(function (elem) {
+//     return elem !== 2; // check if element is not equal to 3
+// });
+// var currentArray = arrayFilterer1();
+// console.log('currentArray', currentArray);
---
+++ cs142-template-processor.js
@@ -0,0 +1,24 @@
+'use strict';
+class Cs142TemplateProcessor {
+    constructor(template) {
+        this.template = template;
+    }
+    fillIn(data) {
+        return this.template.replace(/{{(\w*)}}/g, (_m, key) => data.hasOwnProperty(key) ? data[key] : "")
+    }
+}
+
+var templ_ate = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
+var dateTemplate = new Cs142TemplateProcessor(templ_ate);
+
+//Case: property exist in dictionary
+var dictionary = { month: 'July', day: '1', year: '2016' };
+var str = dateTemplate.fillIn(dictionary);
+
+//Case: property doesn't exist in dictionary
+var dictionary2 = { day: '1', year: '2016' };
+var str2 = dateTemplate.fillIn(dictionary2);
+
+const assert = require('assert');
+assert.deepStrictEqual(str, 'My favorite month is July but not the day 1 or the year 2016');
+assert.deepStrictEqual(str2, 'My favorite month is  but not the day 1 or the year 2016');
